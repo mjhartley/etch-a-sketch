6c3c2469b5356f3ca9fea7f73ab500ae7fd08320
mjhartley <mike@the-hartleys.co.uk>
2024-05-07 15:01:47 +0100

    Final version

--- javascript.js
+++ javascript.js
@@ -9,13 +9,8 @@ Version 1
 const gridContainer = document.querySelector(".gridContainer");
 let gridSize = 0;
 let sqSize = 0;
-// calc padding for .gridBox
-//    width: 5%;
-//    padding-bottom: 5%;
-
 
 // functions 
-
 function drawGrid(gridSize){
 
     // remove existing grid    
@@ -27,34 +22,23 @@ function drawGrid(gridSize){
     sqSize = 960 / gridSize;
     console.log("Square Size:" + sqSize);
 
-    for (rows = 0; rows < gridSize; rows++){
-        gridDiv = document.createElement("div");
-        gridDiv.setAttribute('id', 'r'  + rows);
-        gridDiv.setAttribute('class', 'row');
-        gridContainer.appendChild(gridDiv);
-        console.log("Writing row: " + rows);
+    gridSize = gridSize*gridSize;
 
-    for (columns = 0; columns < gridSize; columns++){
+    for (cells = 0; cells < gridSize; cells++){
     
         gridDiv = document.createElement("div");
-        gridDiv.setAttribute('id', 'c' + columns);
+        gridDiv.setAttribute('id', cells);
         gridDiv.setAttribute('class', 'gridBox');
         gridDiv.style.height = sqSize+'px';
         gridDiv.style.width = sqSize+'px';
         gridContainer.appendChild(gridDiv);
-        console.log("Writing column: " + columns);
-        }
 
-    }      
+    } 
 
     const myDivs = document.querySelectorAll(".gridBox");
 
     myDivs.forEach((div) => {
-
         div.addEventListener('mouseover', function(e) {
-            console.log("Adding Event Listeners")
-            console.log(e.target);
-            console.log(div.id);
             e.target.style.background = "white";
         });
     });
@@ -62,19 +46,19 @@ function drawGrid(gridSize){
 }
 
 function removeGridDivNodes(gridContainer){
-
     console.log("Removing old grid");
-
     while(gridContainer.firstChild){    
         gridContainer.removeChild(gridContainer.lastChild);
     }
 }
 
 const createGridBtn = document.querySelector('#createGridBtn');
-
 createGridBtn.addEventListener('click',()=> {
-
     gridSize = prompt("Enter the width of the grid. Max 100."); 
-    drawGrid(gridSize);
-
+   if ((gridSize > 100) || (gridSize < 1)){
+        alert("Input is invalid");
+    } else {
+   
+        drawGrid(gridSize);
+    }
 });
